90698cb4e144cb0d7a9b53ae61ce5ea555ec3a32
rbaul <4RFVbgt%>
2023-02-14 13:38:53 +0200

    LRU Cache without use LinkedHashMap

--- src/main/java/com/leetcode/medium/LRUCache.java
+++ src/main/java/com/leetcode/medium/LRUCache.java
@@ -1,6 +1,6 @@
 package com.leetcode.medium;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -31,23 +31,135 @@ import java.util.Map;
  * <a href="https://leetcode.com/problems/lru-cache/">LRU Cache</a>
  */
 class LRUCache {
+
+//	private final Map<Integer, Integer> map;
+//
+//	public LRUCache(int capacity) {
+//		map = new LinkedHashMap<>(capacity + 1, 1, true) {
+//			@Override
+//			protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+//				return size() > capacity;
+//			}
+//		};
+//	}
+//
+//	public int get(int key) {
+//		return map.getOrDefault(key, -1);
+//	}
+//
+//	public void put(int key, int value) {
+//		map.put(key, value);
+//	}
 	
-	private final Map<Integer, Integer> map;
+	private final int capacity;
+	
+	public final Map<Integer, LinkedNode> map;
+	
+	public final LinkedNodeList linkedNodeList;
 	
 	public LRUCache(int capacity) {
-		map = new LinkedHashMap<>(capacity + 1, 1, true) {
-			@Override
-			protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-				return size() > capacity;
-			}
-		};
+		this.capacity = capacity;
+		map = new HashMap<>(capacity);
+		linkedNodeList = new LinkedNodeList();
 	}
 	
 	public int get(int key) {
-		return map.getOrDefault(key, -1);
+		LinkedNode linkedNode = map.get(key);
+		if (linkedNode == null) {
+			return -1;
+		} else {
+			linkedNodeList.toFirst(linkedNode);
+			return linkedNode.value;
+		}
 	}
 	
 	public void put(int key, int value) {
-		map.put(key, value);
+		LinkedNode linkedNode = map.get(key);
+		if (linkedNode == null) {
+			if (map.size() == capacity) {
+				LinkedNode linkedNode1 = linkedNodeList.removeLast();
+				if (linkedNode1 != null) {
+					map.remove(linkedNode1.key);
+				}
+			}
+			linkedNode = new LinkedNode(key, value);
+			map.put(key, linkedNode);
+		} else {
+			linkedNode.value = value;
+		}
+		linkedNodeList.toFirst(linkedNode);
+	}
+	
+	
+	private static class LinkedNode {
+		public int key;
+		public int value;
+		public LinkedNode prev;
+		public LinkedNode next;
+		
+		public LinkedNode() {
+		}
+		
+		public LinkedNode(int key, int value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return "{" + key +
+					"=" + value +
+					'}';
+		}
+	}
+	
+	private static class LinkedNodeList {
+		public LinkedNode first;
+		public LinkedNode last;
+		
+		public LinkedNodeList() {
+			first = new LinkedNode();
+			last = new LinkedNode();
+			
+			first.next = last;
+			last.prev = first;
+		}
+		
+		public void toFirst(LinkedNode linkedNode) {
+			LinkedNode prevUsed = linkedNode.prev;
+			if (prevUsed != first) {
+				if (prevUsed != null) { // exist
+					prevUsed.next = linkedNode.next;
+					linkedNode.next.prev = prevUsed;
+				}
+				
+				linkedNode.prev = first;
+				linkedNode.next = first.next;
+				first.next.prev = linkedNode;
+				first.next = linkedNode;
+			}
+		}
+		
+		public LinkedNode removeLast() {
+			LinkedNode toRemove = last.prev;
+			if (toRemove != first) {
+				toRemove.prev.next = last;
+				last.prev = toRemove.prev;
+				return toRemove;
+			} else {
+				return null;
+			}
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder stringBuilder = new StringBuilder();
+			LinkedNode starter = first.next;
+			while (starter != last) {
+				stringBuilder.append(starter.toString());
+				starter = starter.next;
+			}
+			return stringBuilder.toString();
+		}
 	}
 }
--- src/test/java/com/leetcode/medium/LRUCacheTest.java
+++ src/test/java/com/leetcode/medium/LRUCacheTest.java
@@ -2,7 +2,7 @@ package com.leetcode.medium;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LRUCacheTest {
 	
@@ -30,5 +30,91 @@ class LRUCacheTest {
 		lRUCache.put(1, 2); // cache is {1=2}
 		assertEquals(2, lRUCache.get(1));    // return 2
 		assertEquals(6, lRUCache.get(2));    // return 6
+	}
+	
+	@Test
+	void lruCache_3() {
+		LRUCache lRUCache = new LRUCache(1);
+		lRUCache.put(2, 1); // cache is {2=1}
+		assertEquals(1, lRUCache.get(2));    // return 1
+	}
+	
+	@Test
+	void lruCache_4() {
+		LRUCache lRUCache = new LRUCache(2);
+		lRUCache.put(1, 0); // cache is {1=0}
+		lRUCache.put(2, 2); // cache is {2=2, 1=0}
+		assertEquals(0, lRUCache.get(1));    // return 0
+		lRUCache.put(3, 3); // cache is {3=3, 2=2}
+		assertEquals(-1, lRUCache.get(2));    // returns -1 (not found)
+		lRUCache.put(4, 4); // cache is {4=4, 3=3}
+		assertEquals(-1, lRUCache.get(1));    // returns -1 (not found)
+		assertEquals(3, lRUCache.get(3));    // returns 3
+		assertEquals(4, lRUCache.get(4));    // returns 4
+	}
+	
+	@Test
+	void lruCache_5() {
+		LRUCache lRUCache = new LRUCache(10);
+		lRUCache.put(10, 13);
+		lRUCache.put(3, 17);
+		lRUCache.put(6, 11);
+		lRUCache.put(10, 5);
+		lRUCache.put(9, 10);
+		assertEquals(-1, lRUCache.get(13));
+		lRUCache.put(2, 19);
+		assertEquals(19, lRUCache.get(2));
+		assertEquals(17, lRUCache.get(3));
+		lRUCache.put(5, 25);
+		assertEquals(-1, lRUCache.get(8));
+		lRUCache.put(9, 22);
+		lRUCache.put(5, 5);
+		lRUCache.put(1, 30);
+		assertEquals(-1, lRUCache.get(11));
+		lRUCache.put(9, 12);
+		assertEquals(-1, lRUCache.get(7));
+		assertEquals(5, lRUCache.get(5));
+		assertEquals(-1, lRUCache.get(8));
+		assertEquals(12, lRUCache.get(9));
+		lRUCache.put(4, 30);
+		lRUCache.put(9, 3);
+		assertEquals(3, lRUCache.get(9));
+		assertEquals(5, lRUCache.get(10));
+		assertEquals(5, lRUCache.get(10));
+		lRUCache.put(6, 14);
+		lRUCache.put(3, 1);
+		assertEquals(1, lRUCache.get(3));
+		lRUCache.put(10, 11);
+		assertEquals(-1, lRUCache.get(8));
+		lRUCache.put(2, 14);
+		assertEquals(30, lRUCache.get(1));
+		assertEquals(5, lRUCache.get(5));
+		assertEquals(30, lRUCache.get(4));
+		lRUCache.put(11, 4);
+		lRUCache.put(12, 24);
+		lRUCache.put(5, 18);
+		assertEquals(-1, lRUCache.get(13));
+		lRUCache.put(7, 23);
+		assertEquals(-1, lRUCache.get(8));
+		assertEquals(24, lRUCache.get(12));
+		lRUCache.put(3, 27);
+		lRUCache.put(2, 12);
+		assertEquals(18, lRUCache.get(5));
+		lRUCache.put(2, 9);
+		lRUCache.put(13, 4);
+		lRUCache.put(8, 18);
+		lRUCache.put(1, 7);
+		assertEquals(-1, lRUCache.get(6));
+	}
+	
+	@Test
+	void lruCache_6() {
+		LRUCache lRUCache = new LRUCache(2);
+		lRUCache.put(6, 1);
+		lRUCache.put(1, 1);
+		lRUCache.put(6, 1);
+		lRUCache.put(2, 1);
+		lRUCache.put(3, 1);
+		assertEquals(-1, lRUCache.get(6));
 	}
 }
